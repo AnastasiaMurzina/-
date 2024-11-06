8c986e38b891edac27d030ce10765d6ab77ab945
Anastasiy M <ad.murzzzina@gmail.com>
2024-11-06 20:28:39 +0800

    Add files via upload

---
+++ Transporter2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transporter2 : MonoBehaviour
+{
+    public PlayerAttack At;
+    public GameObject Fantom;
+
+
+    void Start()
+    {
+        At = Fantom.GetComponent<PlayerAttack>();
+    }
+
+    public void AttackStart()
+    {
+        At.AttackStart2();
+    }
+
+    void Update()
+    {
+
+    }
+}
